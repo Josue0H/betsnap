6fc2f5b9eac4f2044ad148ff3a03f292833f3c61
Josué Olmos <78229953+Josue0H@users.noreply.github.com>
2024-03-12 22:54:41 -0600

    fix: removed unnecessary controllers

--- test/betsnap_web/controllers/page_controller_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule BetsnapWeb.PageControllerTest do
-  use BetsnapWeb.ConnCase
-
-  test "GET /", %{conn: conn} do
-    conn = get(conn, ~p"/")
-    assert html_response(conn, 200) =~ "Peace of mind from prototype to production"
-  end
-end
